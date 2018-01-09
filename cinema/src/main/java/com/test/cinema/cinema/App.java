c45cb59b401a21c4dac0aece107b265da6d7c1aa
IxTHING <jordan.white191191@yahoo.co.uk>
2018-01-09 21:26:29 +0000

    addedCinema

---
+++ cinema/src/main/java/com/test/cinema/cinema/App.java
@@ -0,0 +1,24 @@
+package com.test.cinema.cinema;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        Movies rambo = new Movies("Rambo", "Action", 18);
+        Movies starWars = new Movies("Star Wars", "Sci-Fi", 12);
+        
+        Service service;
+        service = new Service(rambo);
+        service.addMovie(starWars);
+        service.deleteMovie(1);
+        service.printMap();
+    }
+}
